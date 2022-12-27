50cdd200eaaaccff4d01b18295da33254e1cd270
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-12-27 23:48:37 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { renderTrendingFilms } from './js/renderTrendingFilms';
 import { searchFilms } from './js/searchFilms';
 import { onCardClick } from './js/onCardClick';
 import { getTrailerFilm } from './js/getTrailerFilm';
+import { backToTop } from './js/backToTop';
 
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
@@ -10,21 +11,10 @@ const DEBOUNCE_DELAY = 300;
 export const searchForm = document.querySelector('.search-form-input');
 export const galleryList = document.querySelector('.gallery');
 export const searchResult = document.querySelector('.search-result');
-// const getTrailer = document.querySelector('.gallery-item-trailer');
 
 export const fetchApi = new FetchAPI();
 
 document.addEventListener('click', onCardClick);
 document.addEventListener('DOMContentLoaded', renderTrendingFilms());
 document.addEventListener('click', getTrailerFilm);
-
 searchForm.addEventListener('input', debounce(searchFilms, DEBOUNCE_DELAY));
-
-// export async function getTrailerFilm(e) {
-//   console.log(e);
-//   // movie = await fetchApi.getTrailer(id);
-//   // const { key } = movie.data.results[0];
-//   // console.log(key);
-// }
-
-// // getTrailerFilm(76600);
--- src/js/renderGallery.js
+++ src/js/renderGallery.js
@@ -2,7 +2,6 @@ import { createMarkupElemetsGallery } from './createMarkupElemetsGallery';
 import { galleryList, fetchApi } from '../index.js';
 
 export function renderGallery(galleryEl) {
-  // console.log('galleryEl: ', galleryEl);
   const galleryElements = galleryEl.map(elem =>
     createMarkupElemetsGallery(elem, fetchApi)
   );
